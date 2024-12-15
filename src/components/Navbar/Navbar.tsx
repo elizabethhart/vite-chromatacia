6db293040526a6e379d5624f46ef9d6ce1e87c60
Elizabeth Hart <elizabethwhart@gmail.com>
2024-12-14 23:11:37 -0600

    Style nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import { Link } from "react-router";
+
 import "./Navbar.css";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
-    <nav>
-      <div>
-        <div>
-          <Link to="">elizabethhart.com</Link>
-        </div>
-        <ul role="menu">
+    <nav className="nav" aria-label="Personal Website">
+      <div className="nav__brand">
+        <Link to="">Hello World</Link>
+      </div>
+      <div className="menu">
+        <button
+          className="menu__button"
+          onClick={() => setShowMenu((prev) => !prev)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <ul
+          className="menu__items"
+          role="menu"
+          aria-label="Navigation Menu"
+          aria-expanded={showMenu}
+        >
           <li>
-            <Link role="menu-item" to="">
+            <Link className="menu__item" role="menu-item" to="">
               Home
             </Link>
           </li>
           <li>
-            <Link role="menu-item" to="about">
+            <Link className="menu__item" role="menu-item" to="about">
               About
             </Link>
           </li>
