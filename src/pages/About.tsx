4f79db13b29d925eae426af5fa5415938b6bccdd
Elizabeth Hart <elizabethwhart@gmail.com>
2024-12-15 23:01:18 -0600

    Tailwind config

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,10 @@
 function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h2>Hi, I'm Elizabeth Hart, I'm a Software Engineer.</h2>
+      <p>This is my personal website that I made just for fun.</p>
+    </>
+  );
 }
 
 export default About;
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,16 +1,38 @@
-import { Outlet } from "react-router";
-import Navbar from "../components/Navbar/Navbar";
+import { Link, Outlet } from "react-router";
 
 function Layout() {
   return (
-    <div>
-      <header>
-        <Navbar />
+    <>
+      <header className="flex justify-center">
+        <nav
+          aria-label="Personal Website"
+          className="max-w-screen-lg w-full flex justify-between py-4"
+        >
+          <div>
+            <Link to="">Hello World</Link>
+          </div>
+          <div>
+            <ul className="flex gap-4" role="menu" aria-label="Navigation Menu">
+              <li>
+                <Link role="menu-item" to="">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link role="menu-item" to="about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
-      <main>
-        <Outlet />
+      <main className="flex justify-center h-full">
+        <div className="max-w-screen-lg w-full">
+          <Outlet />
+        </div>
       </main>
-    </div>
+    </>
   );
 }
 
